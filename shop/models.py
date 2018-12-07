ee93931332e36db0ddcd78ee8ca2c41ef2a1e505
ai-rage <azamat.murdalov@mail.ru>
2018-12-07 23:44:34 +0300

    add Product model

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+	name = models.CharField('Название', max_length=100)
+	description = models.CharField('Описание', max_length=200)
+	price = models.CharField('Цена', max_length=100)
+	date = models.DateTimeField('Дата')
+
+	def __str__(self):
+		return self.name
